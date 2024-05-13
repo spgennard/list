82aaa4d3ab8b5568f07ebad296219fa885b51f0c
Stephen Gennard <stephen@gennard.net>
2024-05-13 16:46:15 +0100

    add virt add script

---
+++ bin/virsh_prep_client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "x$(which yum 2>/dev/null)" == "x"  ];
+then
+	sudo apt install libnss-libvirt ## Debian/Ubuntu ##
+else
+	sudo yum install libvirt-nss ## RHEL/CentOS/Fedora ##
+fi
+
+
+if [ "x$(grep -w 'hosts:' /etc/nsswitch.conf | grep libvrt)" == "x" ];
+then
+	echo "/etc/nsswitch.conf need the following added:"
+	echo " libvirt libvirt_guest"
+	echo " in the files area"
+fi
