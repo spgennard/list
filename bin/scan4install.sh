6e7f09112445678d266981cce5a521a351d88f5b
Stephen Gennard <stephen@gennard.net>
2024-06-09 15:05:16 +0100

    start to add acu

--- bin/scan4install.sh
+++ bin/scan4install.sh
@@ -2,7 +2,7 @@
 
 EXIT_OR_RETURN=return
 BASHRC_MODE=yes
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 SCRIPT_RC="$SCRIPT_DIR/scan4install.rc"
 
 # Default To 24 hours
@@ -13,61 +13,54 @@ trap "rm -f $TMP_FILE" EXIT
 
 # If not running interactively, don't do anything
 case $- in
-    *i*) ;;
-      *) 
-		IS_RC=$(echo $- | cut -c1)
-		if [ "$IS_RC" == "-" ];
-		then 
-			return
-		else
-			EXIT_OR_RETURN=exit
-			BASHRC_MODE=no
-		fi
+*i*) ;;
+*)
+	IS_RC=$(echo $- | cut -c1)
+	if [ "$IS_RC" == "-" ]; then
+		return
+	else
+		EXIT_OR_RETURN=exit
+		BASHRC_MODE=no
+	fi
 	;;
 esac
 
 # check whether whiptail or dialog is installed
 # (choosing the first command found)
-read dialog <<< "$(which dialog whiptail 2> /dev/null)"
+read dialog <<<"$(which dialog whiptail 2>/dev/null)"
 
 # exit if none found
 [[ "$dialog" ]] || {
-  echo 'neither whiptail nor dialog found' >&2
-  $EXIT_OR_RETURN
+	echo 'neither whiptail nor dialog found' >&2
+	$EXIT_OR_RETURN
 }
-
 
 ignore_BKP=yes
 SCAN_CACHE_FILE=~/.scan4install.cache
 short_names_filter="sed -e \"s/Micro Focus //\""
 
-function beginswith
-{ 
-	case $2 in 
-		"$1"*) true;; 
-		*) false;; 
-	esac; 
+function beginswith {
+	case $2 in
+	"$1"*) true ;;
+	*) false ;;
+	esac
 }
 
-function pathremove_startswith () {
+function pathremove_startswith() {
 	local IFS=':'
 	local NEWPATH
 	local DIR
 	local PATHVARIABLE=${2:-PATH}
-	for DIR in ${!PATHVARIABLE}
-	do
-		if ! beginswith $1 "$DIR";
-		then
+	for DIR in ${!PATHVARIABLE}; do
+		if ! beginswith $1 "$DIR"; then
 			NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
 		fi
 	done
 	export $PATHVARIABLE="$NEWPATH"
 }
 
-function remove_cobdir
-{
-	if [ ! ".$COBDIR" == "." ];
-	then
+function remove_cobdir {
+	if [ ! ".$COBDIR" == "." ]; then
 		pathremove_startswith $COBDIR PATH
 		pathremove_startswith $COBDIR LD_LIBRARY_PATH
 		pathremove_startswith $COBDIR CLASSPATH
@@ -78,58 +71,64 @@ function remove_cobdir
 	fi
 }
 
-function getScanCacheUpdateTime()
-{
-    local aptDate="$(stat -c %Y $SCAN_CACHE_FILE)"
-    local nowDate="$(date +'%s')"
+function getScanCacheUpdateTime() {
+	local aptDate="$(stat -c %Y $SCAN_CACHE_FILE)"
+	local nowDate="$(date +'%s')"
 
-    echo $((nowDate - aptDate))
+	echo $((nowDate - aptDate))
 }
 
-function grab_mf_info
-{
+function grab_mf_info {
 	declare POSS_COBDIR=$1
 	declare PRODUCT_NAME=
 	declare BITX64=
 	declare TS=$(stat -c "%Y" $POSS_COBDIR)
 	declare VER=0.0.0
 
-	if [ -f $POSS_COBDIR/etc/cobver ];
-	then
+	if [ -f $POSS_COBDIR/etc/cobver ]; then
 		declare PROD_STYLE=mf
-		while read line
-		do
+		while read line; do
 			case $line in
-				PTI=64*) 
-					BITX64=64 
-					;;
-				cobol*)
-					VER=$(echo $line | cut -f2 -d" ")
-					;;
-				PTI=*for*|PTI=*Unix*|PTI=*Visual\ COBOL*|PTI=*Enterprise\ Developer*|*Community\ Edition*)
-					PRODUCT_NAME=$(echo $line | cut -f2- -d= | eval $short_names_filter)
-					;;
+			PTI=64*)
+				BITX64=64
+				;;
+			cobol*)
+				VER=$(echo $line | cut -f2 -d" ")
+				;;
+			PTI=*for* | PTI=*Unix* | PTI=*Visual\ COBOL* | PTI=*Enterprise\ Developer* | *Community\ Edition*)
+				PRODUCT_NAME=$(echo $line | cut -f2- -d= | eval $short_names_filter)
+				;;
 			esac
-		done < $POSS_COBDIR/etc/cobver
+		done <$POSS_COBDIR/etc/cobver
 	fi
 
-	if [ ! "x$PRODUCT_NAME" == "x" ];
-	then
+	if [ ! "x$PRODUCT_NAME" == "x" ]; then
 		echo "$TS,$PROD_STYLE,$POSS_COBDIR,$VER,$PRODUCT_NAME,$BITX64"
 	fi
 }
 
-function scan_mf_dirs
-{
+function grab_acu_info {
+
+	declare POSS_COBDIR=$1
+	declare PRODUCT_NAME=AcuCOBOL
+	declare BITX64=
+	declare TS=$(stat -c "%Y" $POSS_COBDIR)
+	declare VER=0.0.0
+	declare PROD_STYLE=acu
+
+	if [ ! "x$PRODUCT_NAME" == "x" ]; then
+		echo "$TS,$PROD_STYLE,$POSS_COBDIR,$VER,$PRODUCT_NAME,$BITX64"
+	fi
+}
+
+function scan_mf_dirs {
 	declare POSS_COBDIR
 	declare POSS_COBDIRS
 	declare FIND_ARG
-	
-	if [ -s $SCAN_CACHE_FILE ];
-	then
+
+	if [ -s $SCAN_CACHE_FILE ]; then
 		lastScanUpdateTime="$(getScanCacheUpdateTime)"
-		if [[ "${lastScanUpdateTime}" -lt "${UPDATEINTERVAL}" ]];
-		then
+		if [[ "${lastScanUpdateTime}" -lt "${UPDATEINTERVAL}" ]]; then
 			cat $SCAN_CACHE_FILE
 			return
 		fi
@@ -137,43 +136,50 @@ function scan_mf_dirs
 	fi
 
 	# MF
-	if [ ! "x$COBDIR" == "x" ];
-	then
+	if [ ! "x$COBDIR" == "x" ]; then
 		POSS_COBDIRS="$COBDIR/etc/cobver"
-	fi	
+	fi
 
-	if [ ! "x$MFPRODBASE" == "x" ];
-	then
+	if [ ! "x$MFPRODBASE" == "x" ]; then
 		POSS_COBDIRS="$MFPRODBASE/etc/cobver $POSS_COBDIRS"
 	fi
 
-	POSS_COBDIRS="$POSS_COBDIRS $(find ~ -maxdepth 4 -type f $FIND_ARG -ipath '*/etc/cobver' -print 2>/dev/null)"
+	POSS_COBDIRS="$POSS_COBDIRS $(find ~ -maxdepth 3 -type f $FIND_ARG -ipath '*/etc/cobver' -print 2>/dev/null)"
+	POSS_COBDIRS="$POSS_COBDIRS $(find ~ -maxdepth 3 -type f $FIND_ARG -ipath '*/etc/cblconfig' -print 2>/dev/null)"
 	POSS_COBDIRS="$POSS_COBDIRS $(find /opt/microfocus -maxdepth 3 -type f $FIND_ARG -ipath '*/etc/cobver' -print 2>/dev/null)"
 
 	(
-		if [ -s $SCAN_CACHE_FILE ];
-		then
+		if [ -s $SCAN_CACHE_FILE ]; then
 			cat $SCAN_CACHE_FILE
 		fi
 
-		for i in $POSS_COBDIRS
-		do
-			POSS_COBDIR=$(cd $(dirname $i)/..; pwd)
-			case "${ignore_BKP}_${POSS_COBDIR}" in
+		for i in $POSS_COBDIRS; do
+			POSS_COBDIR=$(
+				cd $(dirname $i)/..
+				pwd
+			)
+			case "$i" in
+			*cblconfig)
+				if [ -f $POSS_COBDIR/etc/cblconfig ]; then
+					grab_acu_info $POSS_COBDIR
+				fi
+				;;
+			*cobver)
+				case "${ignore_BKP}_${POSS_COBDIR}" in
 				yes*BKP\.*) continue ;;
+				esac
+				if [ -f $POSS_COBDIR/bin/cobsetenv ]; then
+					grab_mf_info $POSS_COBDIR
+				fi
+				;;
 			esac
-			if [ -f $POSS_COBDIR/bin/cobsetenv ];
-			then
-				grab_mf_info $POSS_COBDIR
-			fi
 		done
 	) | sort -r -t, -k1 | uniq | tee $TMP_FILE
 
 	mv $TMP_FILE $SCAN_CACHE_FILE
 }
 
-function start_shell_or_env
-{
+function start_shell_or_env {
 	saveIFS=$IFS
 	IFS=","
 	set -- $*
@@ -185,23 +191,35 @@ function start_shell_or_env
 	BITX64=$6
 	IFS=$saveIFS
 
-	remove_cobdir	
-	if [ ! -f $POSS_COBDIR/bin/cobsetenv ];
-	then
-		echo "Selection $actual_c not found"
-		return
-	fi
-	
-	if [ "$BASHRC_MODE" == "no" ];
-	then
-		exec bash --rcfile <(echo ". ~/.bashrc; . $POSS_COBDIR/bin/cobsetenv $POSS_COBDIR")
-	else
-		. $POSS_COBDIR/bin/cobsetenv $POSS_COBDIR
-	fi
+	remove_cobdir
+	case "$PROD_STYLE" in
+	mf)
+		if [ ! -f $POSS_COBDIR/bin/cobsetenv ]; then
+			echo "Selection $actual_c not found"
+			return
+		fi
+		if [ "$BASHRC_MODE" == "no" ]; then
+			exec bash --rcfile <(echo ". ~/.bashrc; . $POSS_COBDIR/bin/cobsetenv $POSS_COBDIR")
+		else
+			. $POSS_COBDIR/bin/cobsetenv $POSS_COBDIR
+		fi
+		;;
+	acu)
+		if [ ! -f $POSS_COBDIR/bin/acusetenv.sh ]; then
+			echo "Selection $actual_c not found"
+			return
+		fi
+		if [ "$BASHRC_MODE" == "no" ]; then
+			exec bash --rcfile <(echo ". ~/.bashrc; . $POSS_COBDIR/bin/acusetenv.sh $POSS_COBDIR")
+		else
+			. $POSS_COBDIR/bin/acusetenv.sh $POSS_COBDIR
+		fi
+	;;
+	esac
+
 }
 
-function dialog_mf
-{
+function dialog_mf {
 	declare TS
 	declare PROD_STYLE
 	declare POSS_COBDIR
@@ -209,17 +227,15 @@ function dialog_mf
 	declare PRODUCT_NAME
 	declare BITX64
 	declare prodinfo
-	
+
 	scan_lines=$(scan_mf_dirs | grep -Ev "^$")
-	if [ "x$scan_lines" == "x" ];
-	then
+	if [ "x$scan_lines" == "x" ]; then
 		$EXIT_OR_RETURN
 	fi
 	readarray mf_lines <<<"$scan_lines"
 
 	ch=1
-	for mf_line in "${mf_lines[@]}"
-	do
+	for mf_line in "${mf_lines[@]}"; do
 		saveIFS=$IFS
 		IFS=","
 		prodinfo=($mf_line)
@@ -231,34 +247,31 @@ function dialog_mf
 		VER=${prodinfo[3]}
 		PRODUCT_NAME=${prodinfo[4]}
 		BITX64=${prodinfo[5]}
-		
+
 		echo "$ch = $PRODUCT_NAME"
 		TS_DATE=$(date -u "+%x" -d @$TS)
 		# echo " $TS_DATE -> $POSS_COBDIR"
-		ch=$(( $ch + 1 ))
+		ch=$(($ch + 1))
 
 		# echo "$TS,$PROD_STYLE,$POSS_COBDIR,$VER,$PRODUCT_NAME,$BITX64"
 	done
 
-	if [ ! "${#mf_lines[@]}" == "1" ];
-	then
+	if [ ! "${#mf_lines[@]}" == "1" ]; then
 		read -p 'Which? ' -a c
 	else
 		c=1
 	fi
-	
+
 	actual_c=$c
-	c=$(( $c - 1 ))
-	if [ "x${mf_lines[c]}" == "x" ];
-	then
+	c=$(($c - 1))
+	if [ "x${mf_lines[c]}" == "x" ]; then
 		return
 	fi
 
 	start_shell_or_env ${mf_lines[c]}
 }
 
-function dialog_mf_cu
-{
+function dialog_mf_cu {
 	declare TS
 	declare PROD_STYLE
 	declare POSS_COBDIR
@@ -273,19 +286,17 @@ function dialog_mf_cu
 	declare BOX_LINES
 
 	scan_lines=$(scan_mf_dirs | grep -Ev "^$")
-	if [ "x$scan_lines" == "x" ];
-	then
+	if [ "x$scan_lines" == "x" ]; then
 		$EXIT_OR_RETURN
 	fi
 	readarray mf_lines <<<"$scan_lines"
 
-	BOX_MLIMIT=$(( ${#mf_lines[@]} ))
+	BOX_MLIMIT=$((${#mf_lines[@]}))
 
-	BOX_LINES=$(( BOX_MLIMIT + 6 ))
+	BOX_LINES=$((BOX_MLIMIT + 6))
 
-	if [[ $BOX_LINES -gt $LINES ]];
-	then
-		read dialog <<< "$(which dialog 2> /dev/null)"
+	if [[ $BOX_LINES -gt $LINES ]]; then
+		read dialog <<<"$(which dialog 2>/dev/null)"
 
 		# exit if none found
 		[[ "$dialog" ]] || {
@@ -296,8 +307,7 @@ function dialog_mf_cu
 	# WARGS="--title \"Product?\" --menu "Choose" 20 80 ${#mf_lines[@]}"
 	WARGS="--menu \"Select Product\" $BOX_LINES 80 ${#mf_lines[@]}"
 	ch=1
-	for mf_line in "${mf_lines[@]}"
-	do
+	for mf_line in "${mf_lines[@]}"; do
 		saveIFS=$IFS
 		IFS=","
 		prodinfo=($mf_line)
@@ -309,25 +319,23 @@ function dialog_mf_cu
 		VER=${prodinfo[3]}
 		PRODUCT_NAME=${prodinfo[4]}
 		BITX64=${prodinfo[5]}
-		
+
 		WARGS="$WARGS $ch \"$PRODUCT_NAME\""
 
 		# echo "$ch = $PRODUCT_NAME"
 		TS_DATE=$(date -u "+%x" -d @$TS)
 		# echo " $TS_DATE -> $POSS_COBDIR"
-		ch=$(( $ch + 1 ))
+		ch=$(($ch + 1))
 
 		# echo "$TS,$PROD_STYLE,$POSS_COBDIR,$VER,$PRODUCT_NAME,$BITX64"
 	done
 
-	if [ ! "${#mf_lines[@]}" == "1" ];
-	then
+	if [ ! "${#mf_lines[@]}" == "1" ]; then
 		tput smcup
 		tfile=$$.tmp
 		eval $dialog "$(echo -e $WARGS)" 2>$tfile
 		ret=$?
-		if [ ! "$ret" == "0" ];
-		then
+		if [ ! "$ret" == "0" ]; then
 			tput rmcup
 			cat $tfile
 			return
@@ -339,35 +347,34 @@ function dialog_mf_cu
 		c=1
 	fi
 
-	c=$(( $c - 1 ))
+	c=$(($c - 1))
 
 	start_shell_or_env ${mf_lines[c]}
-	if [ "x${mf_lines[c]}" == "x" ];
-	then
+	if [ "x${mf_lines[c]}" == "x" ]; then
 		return
-	fi	
+	fi
 }
 
 ARGS=$*
-if [ "x$ARGS" == "x" ];
-then
+if [ "x$ARGS" == "x" ]; then
 	dialog_mf_cu
 	$EXIT_OR_RETURN
 fi
 
-for i in $ARGS
-do
+for i in $ARGS; do
 	case "$i" in
-		dialog) dialog=dialog ;;
-		no_short_names_filter) short_names_filter=cat ;;
-		include_bkp_dirs) ignore_BKP= ;;
-		nocache) rm -f $SCAN_CACHE_FILE ;;
-		choose) dialog_mf ;;
-		cu) dialog_mf_cu ;;
-		csv) scan_mf_dirs ;;
-		remove_cobdir) 	remove_cobdir ;;
-		--) ;;
-		*) echo $0: invalid argument $i
-		   $EXIT_OR_RETURN
+	dialog) dialog=dialog ;;
+	no_short_names_filter) short_names_filter=cat ;;
+	include_bkp_dirs) ignore_BKP= ;;
+	nocache) rm -f $SCAN_CACHE_FILE ;;
+	choose) dialog_mf ;;
+	cu) dialog_mf_cu ;;
+	csv) scan_mf_dirs ;;
+	remove_cobdir) remove_cobdir ;;
+	--) ;;
+	*)
+		echo $0: invalid argument $i
+		$EXIT_OR_RETURN
+		;;
 	esac
 done
